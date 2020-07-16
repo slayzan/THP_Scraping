31709bb06a38d3072fb37173c15d2ec1b21b0a28
slayzan <hugo.marquet@gmail.com>
2020-07-16 19:15:18 +0200

    fin enfin

--- lib/depute.rb
+++ lib/depute.rb
@@ -1,16 +1,32 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'open-uri'
 
-
-
-def depute
-    page = Nokogiri::HTML(URI.open('http://www2.assemblee-nationale.fr/deputes/liste/tableau'))
-    tmp = page.css('//table/tbody/tr')
-    tmp.each do |tr|
-        pp hello = tr.css('td[1]/a')['href']
-    end
-    #depute_name = tmp.map {|tr|   { tr.css('td[1]/a').text.delete_prefix("Mme ").delete_prefix("M. ") => tr.css('td[1]/a')['href']}}
-    #depute_name
+def get_email(x)
+    lien = x.css('td[1]/a/@href').text
+    page = Nokogiri::HTML(URI.open("http://www2.assemblee-nationale.fr#{lien}"))
+    email = page.css('.deputes-liste-attributs > dd:nth-child(8) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)').text
 end
 
- depute
+def get_first(x)
+    name = x.css('td[1]/a').text.delete_prefix("Mme ").delete_prefix("M. ")
+    name.split.first
+end
+
+def get_last(x)
+    name = x.css('td[1]/a').text.delete_prefix("Mme ").delete_prefix("M. ")
+    name.split.last
+end
+
+def debuty
+    pages = Nokogiri::HTML(URI.open('http://www2.assemblee-nationale.fr/deputes/liste/tableau'))
+    tmp = pages.css('//table/tbody/tr')
+    debutyname = tmp[0..10].map { |x| { "first_name" => get_first(x), "last_name" => get_last(x), "email" => get_email(x) } }
+end
+
+begin
+    pp debuty
+rescue => e
+    puts 'You messed up in debuty'
+end
