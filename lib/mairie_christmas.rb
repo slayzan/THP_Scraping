332781322584c8aad7a49be1eb24861fa7943c3d
slayzan <hugo.marquet@gmail.com>
2020-07-17 08:29:44 +0200

    commentvar

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -5,8 +5,8 @@ require 'open-uri'
 
 #ouvre la page de la mairie pour recuperer ladresse email
 def get_email(nom)
-    nomCommune = nom['href'].delete_prefix "." 
-    page2 = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com#{nomCommune}"))
+    nom_commune = nom['href'].delete_prefix "." 
+    page2 = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com#{nom_commune}"))
     email = page2.css('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
 end
 
